11c579b90ae1442ada687887ae7587ab03fd16a9
AMIMBA Mickael <mickael.amimba@gmail.com>
2021-04-15 14:29:52 -0300

    creat new function to add value in total array

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 //start
-const newGame = document.querySelector('.new-game ')
+const newGame = document.querySelector('#newGame ')
 //player
 const playerOne = document.querySelector('#playerOne')
 const playerTow = document.querySelector('#playerTow')
@@ -12,15 +12,40 @@ const playerTowGlobal = document.querySelector('#playerTowGlobal')
 
 const rollDice = document.querySelector('#rollDice')
 const hold = document.querySelector('#hold')
-let round = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-let globalPlayerOne =1
+let roundPlayerOne =0
+let roundPlayerTow = 0
+let globalPlayerOne =0
 let globalPlayerTow = 0
+let total =[]
 
 const roundNumber = (max =6, min =1) => {
-    return Math.floor(Math.random() * (6 - 1 + 1)) + 1
+    return Math.floor(Math.random() * (1 + max - min)) + 1
 };
 
 const reset = ()=>{
-    playerOneRound.innerText = globalPlayerOne
+    playerOneRound.innerText = roundPlayerOne
+    playerTowRound.innerText = roundPlayerTow
+    playerOneGlobal.innerText = globalPlayerOne
+    playerTowGlobal.innerText = globalPlayerTow
+    total =[]
 }
 reset()
+
+newGame.addEventListener('click', ()=>{
+    reset()
+})
+rollDice.addEventListener('click',()=>{
+    let iniRound = roundNumber()
+    let ext =[]
+    let next = []
+
+    if (iniRound != 1){
+        ext.push(iniRound)
+            next = ext
+        total.push(next)
+
+        console.log(total)
+        playerOneRound.innerText = total
+    }
+
+})
