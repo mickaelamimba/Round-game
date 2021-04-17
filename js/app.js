bb269b6011e32191854564c05a28a19457e9c981
AMIMBA Mickael <mickael.amimba@gmail.com>
2021-04-17 08:36:26 -0300

    add robot.txt file and info text

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ const cssStyleBg = document.querySelector('.custom-bg-color')
 // button
 const rollDice = document.querySelector('#rollDice')
 const hold = document.querySelector('#hold')
+const info = document.querySelector('#info')
 const roundPlayerOne = 0
 const roundPlayerTow = 0
 const globalPlayerOne = 0
@@ -62,6 +63,17 @@ const restetRound = () => {
   tempTow = []
 }
 
+info.addEventListener('click',()=>{
+  alert(`Le jeu comprend 2 joueurs sur un seul et même écran.
+Chaque joueur possède un score temporaire (ROUND) et un score global (GLOBAL).
+À chaque tour, le joueur a son ROUND initialisé à 0 et peut lancer un dé autant de fois qu'il le souhaite. Le
+résultat d’un lancer est ajouté au ROUND.
+Lors de son tour, le joueur peut décider à tout moment de:
+- Cliquer sur l’option “Hold”, qui permet d’envoyer les points du ROUND vers le GLOBAL. Ce sera alors le
+tour de l’autre joueur.
+- Lancer le dé. S’il obtient un 1, son score ROUND est perdu et c’est la fin de son tour.
+Le premier joueur qui atteint les 100 points sur global gagne le jeu.`)
+})
 newGame.addEventListener('click', () => {
   reset()
 })
